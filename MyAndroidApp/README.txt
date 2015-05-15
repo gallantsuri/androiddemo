c27c1798836fc4135b0834fd2ca62421a72377ae
Suri <suri24@gmail.com>
2015-05-15 15:40:09 -0400

    Update with resource bundle!

---
+++ MyAndroidApp/README.txt
@@ -0,0 +1,3 @@
+This is the demo application on click of Show Dialog button it displays custom dialog with list of values.
+
+@Author: Bhasuri.
--- MyAndroidApp/src/com/suri/android/MainActivity.java
+++ MyAndroidApp/src/com/suri/android/MainActivity.java
@@ -11,21 +11,26 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+/**
+ * A Simple MainActivity class loads list dialog on click of button.
+ * 
+ * @author Suri
+ *
+ */
 public class MainActivity extends Activity {
 
-	final Context context = this;
-	private Button button1, button2;
+	final Context context = this;;
 
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		// choose the button
-		button2 = (Button) findViewById(R.id.buttonShowAlertDialog);
+		Button button = (Button) findViewById(R.id.buttonShowAlertDialog);
 
 		// add button listener
-		button2.setOnClickListener(new OnClickListener() {
+		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				showAlertDialog();
@@ -33,27 +38,30 @@ public class MainActivity extends Activity {
 		});
 
 	}
-	
+
+	/*
+	 * To display List dialog
+	 */
 	private void showAlertDialog() {
-		int start=1, end = 20; // size of the array
-		int size=end-start+1; // size of the array
-		Integer array[] = new Integer[size]; // initialize array
-		for (int i=0; i<size; i++) {
-			// fill the array with serial numbers by increasing start
-			array[i]=start++; 
-		}
-		
-		// displaying alert dialog with list of numbers 
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+		// you can define your own array here 
+		// for String[] array = {"1", "2", "3", "4", "5"}
+		// or you can get it from resource file string.xml
+		String[] array = getResources().getStringArray(R.array.number_array);
+
 		LayoutInflater inflater = getLayoutInflater();
 		View convertView = (View) inflater.inflate(R.layout.dialog, null);
-		alertDialog.setView(convertView);
-		alertDialog.setTitle("List");
-		ListView lv = (ListView) convertView.findViewById(R.id.listView1);
-		ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, array);
-		lv.setAdapter(adapter);
 		
+		// displaying alert dialog with list of numbers
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+		alertDialog.setView(convertView);
+
+		alertDialog.setTitle(getResources().getString(R.string.choose));
+		
+		ListView lv = (ListView) convertView.findViewById(R.id.listView);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
+		lv.setAdapter(adapter);
+
 		alertDialog.show();
 	}
-	
+
 }
